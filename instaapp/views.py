ee815381f72660ced42842c046709d1f132bc539
Paivy <paivyeshirera@gmail.com>
2022-06-08 08:23:16 +0300

    defined the index function

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+import datetime as dt
 
 # Create your views here.
+
+def index(request):
+    profile = Profile.objects.all()
+
+    posts = Image.objects.all()
+    form=CommentForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request,( "Comment has been saved"))
+
+    return render(request, 'index.html',{'posts': posts, 'form': form,'profile': profile,})
