6e3bf2e7534f81950b8fb7f63c294b15df4df189
Paivy <paivyeshirera@gmail.com>
2022-06-08 08:34:15 +0300

    deleted the imported view and modified file

--- instaproject/urls.py
+++ instaproject/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,re_path
-from django.conf.urls.static import static
-from . import views 
+from django.urls import path
+
 
 
 urlpatterns = [
